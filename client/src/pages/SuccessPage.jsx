d565c665141e5883fd7d9487758565c301efc084
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-10-29 14:00:42 +0700

    styling success page

--- client/src/pages/SuccessPage.jsx
+++ client/src/pages/SuccessPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import "./style.css"
 
 function SuccessPage() {
     const history = useHistory()
@@ -10,9 +11,22 @@ function SuccessPage() {
     }
 
     return (
-        <div>
-            <h1>This is Success page</h1>
-            <button onClick={donateAgain}> Donate Again</button>
+        <div className="flex justify-center items-center w-full h-screen bg-green-200">
+            <div className="w-5/6 md:w-1/3 flex flex-col gap-y-2 items-center bg-gray-100 rounded-md shadow-2xl p-8 m-4">
+                <div className="img">
+                <i className="fas fa-check fa-7x"></i>
+                </div>
+
+                <h2 className="text-center uppercase font-semibold text-lg">
+                    Thank you for your donation
+                </h2>
+
+                <h5 className="leading-loose">
+                    Your Donation Has Been Submitted !
+                </h5>
+
+                <button onClick={donateAgain} type="button" className="bg-green-500 mt-6 text-white px-6 py-2 rounded font-medium mx-3 hover:bg-green-600 transition duration-200 each-in-out">Donate Again</button>
+            </div>
         </div>
     )
 }
