74c6a4c8143fb67c276fb4cfb136aa3d61e42bb2
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-10-30 09:19:51 +0700

    setting environment

---
+++ server/app.js
@@ -0,0 +1,20 @@
+// defining stuffs
+require('dotenv/config');
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express();
+const error = require('./middlewares/errorHandler');
+const route = require('./routers/index')
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(urlencoded({extended:true}))
+
+app.use(route)
+
+// error handler
+app.use(error)
+
+app.listen(PORT,() => {
+    console.log(`Server running at port ${PORT}`)
+})
